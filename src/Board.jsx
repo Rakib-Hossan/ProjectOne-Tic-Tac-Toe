3875ecea124978b13e439dd3501581655b241cad
Rakib-Hossan <rakib.redoy01@gmail.com>
2024-10-08 11:17:45 +0600

    make TicTacToe board andsquare

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,22 +1,31 @@
-function Square() {
+function Square({ value }) {
   return (
-    <button className="bg-white border border-gray-800 h-12 w-12 m-1 leading-9 text-lg">
-      X
+    <button className="bg-white border border-gray-800 h-14 w-14 m-1 leading-9 font-bold text-lg">
+      {value}
     </button>
   );
 }
+
 export default function Board() {
   return (
-    <div className="grid grid-cols-3 h-44 w-44">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-    </div>
+    <>
+      <div>
+        <Square value="1" />
+        <Square value="1" />
+        <Square value="1" />
+      </div>
+
+      <div>
+        <Square value="1" />
+        <Square value="1" />
+        <Square value="1" />
+      </div>
+
+      <div>
+        <Square value="1" />
+        <Square value="1" />
+        <Square value="1" />
+      </div>
+    </>
   );
 }
